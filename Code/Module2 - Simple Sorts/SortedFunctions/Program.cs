f97705fe6b87d1c669d288d0970bbffe5c383e82
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-03 20:44:26 +0300

    Module2 -> Selection Sort

--- Code/Module2 - Simple Sorts/SortedFunctions/Program.cs
+++ Code/Module2 - Simple Sorts/SortedFunctions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace SortedFunctions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("using Bubble Sort");
             #region Bubble Sort
             int[] arr = { 5, 2, 9, 1, 5, 6 };
             Console.WriteLine("Original array: " + string.Join(", ", arr));
@@ -18,6 +20,25 @@ namespace SortedFunctions
 
             Console.WriteLine("Sorted array: " + string.Join(", ", arr));
 
+            #endregion
+
+            Console.WriteLine("using Selection Sort");
+            #region Selection Sort
+            int[] arr2 = { 5, 2, 9, 1, 5, 6 };
+
+            Console.WriteLine("Original array: " + string.Join(", ", arr2));
+
+            SelectionSortASC(arr2);
+
+            Console.WriteLine("Sorted array: " + string.Join(", ", arr2));
+
+
+            int[] arr3 = { 5, 2, 9, 1, 5, 6 };
+            Console.WriteLine("Original array: " + string.Join(", ", arr3));
+
+            SelectionSortDESC(arr3);
+
+            Console.WriteLine("Sorted array: " + string.Join(", ", arr3));
             #endregion
         }
 
@@ -31,6 +52,43 @@ namespace SortedFunctions
                     {
                         (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
                     }
+            }
+        }
+
+        //Function for perform Selection Sort
+        static void SelectionSortASC(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for(int j= i + 1; j<n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                //swaping
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+            }
+        }
+
+        static void SelectionSortDESC(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] > arr[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+                //swaping
+                (arr[i], arr[maxIndex]) = (arr[maxIndex], arr[i]);
             }
         }
     }
