15e65e6dc20d95204a6473346bc3328b1fec7fea
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-04 16:17:14 +0300

    Module3 -> BST Implementation

---
+++ Code/Module3 - Binary Search Tree (BST)/BST/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    internal class Program
+    {
+        class BinarySearchTreeNode<T> where T : IComparable<T>
+        {
+            public T Value { get; set; }
+            public BinarySearchTreeNode<T> Left { get; set; }
+            public BinarySearchTreeNode<T> Right { get; set; }
+            public BinarySearchTreeNode(T value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        class BinarySearchTree<T> where T : IComparable<T>
+        {
+            public BinarySearchTreeNode<T> Root { get; private set; }
+
+            //define default constructor
+            public BinarySearchTree()
+            {
+                Root = null;
+            }
+
+            public void Insert(T value)
+            {
+                Root = Insert(Root, value);
+            }
+
+            private BinarySearchTreeNode<T> Insert(BinarySearchTreeNode<T> node, T value)
+            {
+                //base case: if the node is null, create a new node with the value and return it
+                if (node == null)
+                {
+                    return new BinarySearchTreeNode<T>(value);
+                }
+                else if(value.CompareTo(node.Value) < 0)//if the value is less than the node's value, insert it into the left subtree
+                {
+                    node.Left = Insert(node.Left, value);
+                }
+                else if(value.CompareTo(node.Value) > 0)//if the value is greater than the node's value, insert it into the right subtree
+                {
+                    node.Right = Insert(node.Right, value);
+                }
+                return node;
+            }
+
+            public void PrintTree()
+            {
+                PrintTree(Root, 0);
+            }
+
+
+            private void PrintTree(BinarySearchTreeNode<T> root, int space)
+            {
+                int COUNT = 10;  // Distance between levels
+                if (root == null)
+                    return;
+
+                space += COUNT;
+                PrintTree(root.Right, space);
+
+                Console.WriteLine();
+                for (int i = COUNT; i < space; i++)
+                    Console.Write(" ");
+                Console.WriteLine(root.Value);
+                PrintTree(root.Left, space);
+            }
+        }
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+            bst.Insert(45);
+            bst.Insert(15);
+            bst.Insert(79);
+            bst.Insert(90);
+            bst.Insert(10);
+            bst.Insert(55);
+            bst.Insert(12);
+            bst.Insert(20);
+            bst.Insert(50);
+
+            bst.PrintTree();
+
+        }
+    }
+}
---
+++ Code/Module3 - Binary Search Tree (BST)/BST/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BST")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("358c00dd-6f84-420a-982d-7a4bc66e3ff5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
