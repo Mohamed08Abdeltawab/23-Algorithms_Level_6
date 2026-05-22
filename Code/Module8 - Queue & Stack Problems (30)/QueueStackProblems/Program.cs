172cda1867bbff24c0e18bd2ba7980cb02990667
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-23 01:28:10 +0300

    Module7 -> Problem 5 Traffic Signal System Simulation

--- Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Program.cs
+++ Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Program.cs
@@ -8,49 +8,6 @@ namespace QueueStackProblems // 1. browser back button
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            /*
-                        #region Problem 1: browser back button
-                        Stack<string> BrowserHistory = new Stack<string>();
-                        BrowserForwardButton(BrowserHistory, "www.google.com");
-                        BrowserForwardButton(BrowserHistory, "www.facebook.com");
-                        BrowserBackButton(BrowserHistory);
-                        BrowserForwardButton(BrowserHistory, "www.twitter.com");
-                        BrowserBackButton(BrowserHistory);
-                        #endregion
-            */
-            /*
-
-            #region Problem 2: convert decimal to binary using stack
-            Console.WriteLine(DecimalToBinary(10));
-
-            #endregion
-*/
-
-            #region problem 4: queue for printer job
-            Queue<string> queue = new Queue<string>();
-            queue.Enqueue("a");
-            queue.Enqueue("b");
-            queue.Enqueue("c");
-            queue.Enqueue("d");
-            queue.Enqueue("e");
-            queue.Enqueue("f");
-
-            while (queue.Count > 0)
-            {
-                Console.WriteLine("processing..." + queue.Dequeue());
-                if (queue.Count > 0)
-                {
-                    Console.WriteLine("Next job: " + queue.Peek());
-                }
-            }
-
-
-            #endregion
-
-
-        }
         static void BrowserBackButton(Stack<string> BrowserHistory)
         {
             BrowserHistory.Pop();
@@ -80,5 +37,96 @@ namespace QueueStackProblems // 1. browser back button
             }
             return string.Join("", binaryStack);
         }
+
+        static void Main(string[] args)
+        {
+            #region Problem 1: browser back button
+            /*
+                        Stack<string> BrowserHistory = new Stack<string>();
+                        BrowserForwardButton(BrowserHistory, "www.google.com");
+                        BrowserForwardButton(BrowserHistory, "www.facebook.com");
+                        BrowserBackButton(BrowserHistory);
+                        BrowserForwardButton(BrowserHistory, "www.twitter.com");
+                        BrowserBackButton(BrowserHistory);
+            */
+            #endregion
+
+            #region Problem 2: convert decimal to binary using stack
+            /*
+            Console.WriteLine(DecimalToBinary(10));
+
+            */
+            #endregion
+
+            #region problem 4: queue for printer job
+            /*
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+            queue.Enqueue("e");
+            queue.Enqueue("f");
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine("processing..." + queue.Dequeue());
+                if (queue.Count > 0)
+                {
+                    Console.WriteLine("Next job: " + queue.Peek());
+                }
+            }
+            */
+            #endregion
+
+
+            #region problem 5: Traffic Signal System Simulation
+
+            Queue<string> trafficQueue = new Queue<string>();
+            trafficQueue.Enqueue("Car 1");
+            trafficQueue.Enqueue("Truck 1");
+            trafficQueue.Enqueue("Bike 1");
+            trafficQueue.Enqueue("Bus 1");
+
+            while(trafficQueue.Count > 0)
+            {
+                Console.WriteLine(trafficQueue.Dequeue() + " has passed");
+                if(trafficQueue.Count > 0)
+                {
+                    Console.WriteLine("Vehicles waiting...");
+                    Console.WriteLine(string.Join(", ", trafficQueue));
+                }
+                else
+                {
+                    Console.WriteLine("No Vehicles waiting");
+                }
+
+            }
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
     }
 }
