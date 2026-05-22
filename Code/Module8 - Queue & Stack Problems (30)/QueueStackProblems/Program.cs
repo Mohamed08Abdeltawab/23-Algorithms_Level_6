452455ef76f9629978826b04da7d2838f457cd99
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-22 22:34:33 +0300

    Module7 -  browser back button - my solution

---
+++ Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1 // 1. browser back button
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> BrowserHistory = new Stack<string>();
+
+            BrowserForwardButton(BrowserHistory, "www.google.com");
+            BrowserForwardButton(BrowserHistory, "www.facebook.com");
+            BrowserBackButton(BrowserHistory);
+            BrowserForwardButton(BrowserHistory, "www.twitter.com");
+            BrowserBackButton(BrowserHistory);
+
+        }
+        static void BrowserBackButton(Stack<string> BrowserHistory)
+        {
+            BrowserHistory.Pop();
+            if (BrowserHistory.Count <= 0)
+            {
+                Console.WriteLine("No pages in history");
+                return;
+            }
+            Console.WriteLine("\ngo page back");
+            Console.WriteLine("\ncurrent page: " + BrowserHistory.Peek());
+        }
+
+        static void BrowserForwardButton(Stack<string> BrowserHistory, string newPage)
+        {
+            BrowserHistory.Push(newPage);
+            Console.WriteLine("\nOpening page: " + BrowserHistory.Peek());
+        }
+    }
+}
---
+++ Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QueueStackProblems")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QueueStackProblems")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7486f262-5e16-4cde-8795-11ced4f45ee9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
