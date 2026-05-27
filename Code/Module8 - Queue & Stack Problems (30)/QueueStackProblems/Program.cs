67e12585c94c0f88a46b572cecf4651074fe1c0d
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-27 23:27:19 +0300

    Module8 -> Problem 19: Rearrange Queue Alternately

--- Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Program.cs
+++ Code/Module8 - Queue & Stack Problems (30)/QueueStackProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -424,7 +425,7 @@ namespace QueueStackProblems // 1. browser back button
             #endregion
 
             #region Problem 18: Queue using two stacks 
-
+/*
             Stack<int> stack1 = new Stack<int>();
             Stack<int> stack2 = new Stack<int>();
 
@@ -436,7 +437,34 @@ namespace QueueStackProblems // 1. browser back button
                 stack2.Push(stack1.Pop());
 
             Console.WriteLine("Queue from stack1 and stack2: " + string.Join(", ", stack2));
-            
+*/
+
+            #endregion
+
+
+            #region Problem 19: Rearrange Queue Alternately
+
+            //spet1: splite queue to two halves
+            //step2: merge the havles alternately
+            Queue<int> queue = new Queue<int>(new[] {1,2,3,4,5,6});
+            Console.WriteLine("Queue after Alternately: " + string.Join(", ", queue));
+
+            List<int> list = new List<int>(queue);
+            int n = list.Count;
+            Queue<int> result = new Queue<int>();
+
+            for(int i = 0; i < n / 2; i++)
+            {
+                result.Enqueue(list[i]);
+                result.Enqueue(list[n - i -1]);
+            }
+
+            if(n%2 != 0)//if n was odd number so we not taken that in for loop so we aded in the last of result
+                result.Enqueue(list[n/2]);
+
+
+            Console.WriteLine("Queue after Alternately: " + string.Join(", ", result));
+
 
             #endregion
 
@@ -449,6 +477,6 @@ namespace QueueStackProblems // 1. browser back button
 
         }
 
-        
+
     }
 }
