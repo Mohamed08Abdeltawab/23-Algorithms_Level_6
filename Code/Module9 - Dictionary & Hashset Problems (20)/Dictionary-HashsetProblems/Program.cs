2ae15ee555f0bd3122aa51d15496cbc09014a49d
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-01 17:22:44 +0300

    Module9 -> Problem 3: Translate Words Between Languages

--- Code/Module9 - Dictionary & Hashset Problems (20)/Dictionary-HashsetProblems/Program.cs
+++ Code/Module9 - Dictionary & Hashset Problems (20)/Dictionary-HashsetProblems/Program.cs
@@ -31,20 +31,40 @@ namespace Dictionary_HashsetProblems
 
 
             #region Problem 2: Store Book Information
+            /*
+                        Dictionary<int, (string Title, string Author)> BokkInfo = new Dictionary<int, (string Title, string Author)>()
+                        {
+                            { 1, ("The Great Gatsby", "F. Scott Fitzgerald") },
+                            { 2, ("To Kill a Mockingbird", "Harper Lee") },
+                            { 3, ("1984", "George Orwell") },
+                            { 4, ("Pride and Prejudice", "Jane Austen") }
+                        };
 
-            Dictionary<int, (string Title, string Author)> BokkInfo = new Dictionary<int, (string Title, string Author)>()
+                        Console.WriteLine("Book Information:");
+                        foreach(var book in BokkInfo)
+                        {
+                            Console.WriteLine($"Book ID: {book.Key}, Title: {book.Value.Title}, Author: {book.Value.Author}");
+                        }
+            */
+            #endregion
+
+
+            #region Problem 3: Translate Words Between Languages
+
+            Dictionary<string, string> Translate = new Dictionary<string, string>()
             {
-                { 1, ("The Great Gatsby", "F. Scott Fitzgerald") },
-                { 2, ("To Kill a Mockingbird", "Harper Lee") },
-                { 3, ("1984", "George Orwell") },
-                { 4, ("Pride and Prejudice", "Jane Austen") }
+                {"Hello in Spanish", "Hola" },
+                {"Goodbye in French", "Au revoir" },
+                {"Thank you in German", "Danke" },
+                {"Please in Italian", "Per favore" }
             };
 
-            Console.WriteLine("Book Information:");
-            foreach(var book in BokkInfo)
+            Console.WriteLine("Translations:");
+            foreach(var translation in Translate)
             {
-                Console.WriteLine($"Book ID: {book.Key}, Title: {book.Value.Title}, Author: {book.Value.Author}");
+                Console.WriteLine($"Phrase: {translation.Key}, Translation: {Translate[translation.Key]}");
             }
+
 
             #endregion
         }
