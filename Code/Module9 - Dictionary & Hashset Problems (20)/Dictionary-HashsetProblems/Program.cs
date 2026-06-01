8efcb7fae90a7555c77044f388747ad421eeac89
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-01 17:16:52 +0300

    Module9 ->  Problem 2: Store Book Information

--- Code/Module9 - Dictionary & Hashset Problems (20)/Dictionary-HashsetProblems/Program.cs
+++ Code/Module9 - Dictionary & Hashset Problems (20)/Dictionary-HashsetProblems/Program.cs
@@ -11,21 +11,40 @@ namespace Dictionary_HashsetProblems
         static void Main(string[] args)
         {
             #region Problem 1: Store and Retrieve Student Grades
+            /*
+                        Dictionary<string, double> studentGrades = new Dictionary<string, double>()
+                        {
+                            {"Alice", 85.5},
+                            {"Bob", 92.0},
+                            {"Charlie", 78.0},
+                            {"Diana", 88.5 }
+                        };
 
-            Dictionary<string, double> studentGrades = new Dictionary<string, double>()
+                        Console.WriteLine("Student Grades:");
+                        foreach(var student in studentGrades)
+                        {
+                            Console.WriteLine("Student: " + student.Key + ", Grade: " + student.Value);
+                        }
+            */
+
+            #endregion
+
+
+            #region Problem 2: Store Book Information
+
+            Dictionary<int, (string Title, string Author)> BokkInfo = new Dictionary<int, (string Title, string Author)>()
             {
-                {"Alice", 85.5},
-                {"Bob", 92.0},
-                {"Charlie", 78.0},
-                {"Diana", 88.5 }
+                { 1, ("The Great Gatsby", "F. Scott Fitzgerald") },
+                { 2, ("To Kill a Mockingbird", "Harper Lee") },
+                { 3, ("1984", "George Orwell") },
+                { 4, ("Pride and Prejudice", "Jane Austen") }
             };
 
-            Console.WriteLine("Student Grades:");
-            foreach(var student in studentGrades)
+            Console.WriteLine("Book Information:");
+            foreach(var book in BokkInfo)
             {
-                Console.WriteLine("Student: " + student.Key + ", Grade: " + student.Value);
+                Console.WriteLine($"Book ID: {book.Key}, Title: {book.Value.Title}, Author: {book.Value.Author}");
             }
-
 
             #endregion
         }
