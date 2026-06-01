02c74fe435a2abc0a5d0b9a355875c770f4d0630
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-01 17:11:58 +0300

    Module9 -> Problem 1: Store and Retrieve Student Grades

---
+++ Code/Module9 - Dictionary & Hashset Problems (20)/Dictionary-HashsetProblems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_HashsetProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem 1: Store and Retrieve Student Grades
+
+            Dictionary<string, double> studentGrades = new Dictionary<string, double>()
+            {
+                {"Alice", 85.5},
+                {"Bob", 92.0},
+                {"Charlie", 78.0},
+                {"Diana", 88.5 }
+            };
+
+            Console.WriteLine("Student Grades:");
+            foreach(var student in studentGrades)
+            {
+                Console.WriteLine("Student: " + student.Key + ", Grade: " + student.Value);
+            }
+
+
+            #endregion
+        }
+    }
+}
