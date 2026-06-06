9b70c57e9eaead562fc34ff7145ed12cf8f3b9f4
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 16:45:11 +0300

    Module10 -> Problem9_Find_Count_Less_Than_or_Equal

---
+++ Code/Module10 - Sorted List Problems (11)/Problem9-Find Count Less Than or Equal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9_Find_Count_Less_Than_or_Equal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> sortedSet = new SortedSet<int>() { 1, 2, 3, 4, 5, 6 };
+            int value = 4;
+            int count = sortedSet.GetViewBetween(sortedSet.Min, value).Count;
+            Console.WriteLine($"Count of elements less than or equal to {value}: {count}");
+
+            int count2 = sortedSet.Where(x => x <= value).Count();
+            Console.WriteLine($"Count of elements less than or equal to {value} using LINQ: {count2}");
+            
+            int count3 = sortedSet.Count(x => x <= value);
+            Console.WriteLine($"Count of elements less than or equal to {value} using Count method: {count3}");
+
+            int count4 = sortedSet.TakeWhile(x => x <= value).Count();
+            Console.WriteLine($"Count of elements less than or equal to {value} using TakeWhile: {count4}");
+
+            
+        }
+    }
+}
