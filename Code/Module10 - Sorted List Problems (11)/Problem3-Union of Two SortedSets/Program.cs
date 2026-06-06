06d0c18e56998003916062575a1d236d01bcc8b8
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 15:45:57 +0300

    Module10 -> Problem3_Union_of_Two_SortedSets

---
+++ Code/Module10 - Sorted List Problems (11)/Problem3-Union of Two SortedSets/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_Union_of_Two_SortedSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> firstSet = new SortedSet<int>(new[] { 1, 2, 3});
+            SortedSet<int> secondSet = new SortedSet<int>(new[] { 2, 3, 4, 5});
+
+            SortedSet<int> unionSet = new SortedSet<int>(firstSet);
+
+            unionSet.UnionWith(secondSet);
+
+            Console.WriteLine(string.Join(" ", unionSet));
+        }
+    }
+}
