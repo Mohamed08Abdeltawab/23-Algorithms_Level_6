ff1510454eb86a6dff500429eaa114310881a956
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 15:03:31 +0300

    Module10 -> Problem1_FindElementInRange

---
+++ Code/Module10 - Sorted List Problems (11)/Problem1-FindElementInRange/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_FindElementInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> sortedSet = new SortedSet<int>();
+            sortedSet.Add(1);
+            sortedSet.Add(2);
+            sortedSet.Add(3);
+            sortedSet.Add(4);
+            sortedSet.Add(5);
+            sortedSet.Add(6);
+
+            
+
+            List<int> result = GetViewBetweenFunc(sortedSet, 2, 4);
+            Console.WriteLine("Elements between 2 and 4: " + string.Join(", ", result));
+
+            //or just using methods of SortedList
+            var range = sortedSet.GetViewBetween(2, 4);
+
+            Console.WriteLine("Elements between 2 and 4 using GetViewBetween: " + string.Join(", ", range));
+        }
+
+        static List<int> GetViewBetweenFunc(SortedSet<int> sortedSet, int form, int to)
+        {
+            List<int> result = new List<int>();
+            foreach(var item in sortedSet)
+            {
+                if (item >= form && item <= to)
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
