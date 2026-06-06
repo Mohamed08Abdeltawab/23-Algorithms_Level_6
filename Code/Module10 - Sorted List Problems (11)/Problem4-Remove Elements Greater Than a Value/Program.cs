246b0c7b4c9568ea9cbe443c9c5efc1d567786d4
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 15:56:52 +0300

    Module10 -> Problem4_Remove_Elements_Greater_Than_a_Value

---
+++ Code/Module10 - Sorted List Problems (11)/Problem4-Remove Elements Greater Than a Value/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_Remove_Elements_Greater_Than_a_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> set = new SortedSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int value = 3;
+
+            for (int i = 0;i< set.Count;i++)
+            {
+                if(set.ElementAt(i) > value)
+                {
+                    set.Remove(set.ElementAt(i));
+                    i--; // Adjust the index after removal
+                }
+            }
+            Console.WriteLine(string.Join(", ", set));
+
+            // or using getviewbetween
+            set = set.GetViewBetween(set.Min, value);
+            Console.WriteLine(string.Join(", ", set));
+        }
+    }
+}
