54fcf48edf49be1c371be8da9a90ee56a35bc1ac
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 16:31:11 +0300

    Module10 -> Problem8_Find_All_Elements_Not_in_a_Range

---
+++ Code/Module10 - Sorted List Problems (11)/Problem8-Find All Elements Not in a Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8_Find_All_Elements_Not_in_a_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> set = new SortedSet<int>(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            var result = GetElementNotInRange(set, 3, 7).ToList();
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        static IEnumerable<int> GetElementNotInRange(SortedSet<int> set, int start, int end)
+        {
+            var range = set.GetViewBetween(start, end);
+            SortedSet<int> resultSet = new SortedSet<int>(set);
+            resultSet.ExceptWith(range);
+            return resultSet;
+        }
+    }
+}
