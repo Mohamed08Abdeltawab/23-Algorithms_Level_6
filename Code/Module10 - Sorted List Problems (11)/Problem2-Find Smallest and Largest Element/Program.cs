3e622e18ee6ddee65634b1845efc7481938364a2
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 15:06:13 +0300

    Module10 -> Problem2_Find_Smallest_and_Largest_Element

---
+++ Code/Module10 - Sorted List Problems (11)/Problem2-Find Smallest and Largest Element/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_Find_Smallest_and_Largest_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> set = new SortedSet<int>(new[] { 1, 2, 6, 4, 5, 3});
+
+            Console.WriteLine("Smallest element: " + set.Min);
+            Console.WriteLine("Largest element: " + set.Max);
+        }
+    }
+}
