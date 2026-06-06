b700a38245fea87c6d27b24f072eb38ff41b86c9
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 17:05:00 +0300

    Module10 -> Problem10_Real_Time_Leaderboard {

---
+++ Code/Module10 - Sorted List Problems (11)/Problem10-Real-Time Leaderboard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem10_Real_Time_Leaderboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, int> leaderboard = new SortedList<string, int>()
+            {
+                {"Alice", 100},
+                {"Bob", 150},
+                {"Charlie", 120}
+             };
+
+
+            Console.WriteLine("Initial Leaderboard:");
+            foreach (var entry in leaderboard)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+    }
+}
