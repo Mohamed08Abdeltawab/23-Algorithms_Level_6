d43950ae11c62da9e3ecdf9507b86bc8705237d2
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-06 16:20:50 +0300

    Module10 -> Problem7_Remove_All_Elements_Within_a_Range

---
+++ Code/Module10 - Sorted List Problems (11)/Problem7-Remove All Elements Within a Range/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7_Remove_All_Elements_Within_a_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> set = new SortedSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            SortedSet<int> cpyset = new SortedSet<int>(set);
+
+            set.RemoveWhere(x => x >= 3 && x <= 7);
+
+            cpyset.GetViewBetween(3, 7).Clear();
+
+            Console.WriteLine(string.Join(", ", set));
+            Console.WriteLine(string.Join(", ", cpyset));
+        }
+    }
+}
