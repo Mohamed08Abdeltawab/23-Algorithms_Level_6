5f8e6fcf84d2e4c13d7994c6c37c8406fbc80842
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-07 17:01:44 +0300

    Module11 -> Problem2_Shopping_Cart_for_an_E_commerce_App

---
+++ Code/Module11- Observable Collection Problems (7)/Problem2-Shopping Cart for an E-commerce App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_Shopping_Cart_for_an_E_commerce_App
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> cart = new ObservableCollection<string>();
+            cart.CollectionChanged += (sender, e) =>
+            {
+                if(e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"Item {e.NewItems[0]} added to cart");
+                if(e.Action == NotifyCollectionChangedAction.Remove)
+                    Console.WriteLine($"Item {e.OldItems[0]} removed from cart");
+                if(e.Action == NotifyCollectionChangedAction.Replace)
+                    Console.WriteLine($"Item {e.OldItems[0]} replaced with {e.NewItems[0]} in cart");
+                if (e.Action == NotifyCollectionChangedAction.Reset)
+                    Console.WriteLine("Cart cleared");
+            };
+
+            cart.Add("Laptop");
+            cart.Add("Smartphone");
+            cart.Remove("Laptop");
+            cart[0] = "Tablet";
+        }
+    }
+}
