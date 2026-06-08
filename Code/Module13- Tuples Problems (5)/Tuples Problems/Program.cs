bffced24ce5cc2f18366d1ca1d121c20d46a9af6
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-08 19:59:52 +0300

    Module13 -> Problem 1: Return Multiple Values from a Function

---
+++ Code/Module13- Tuples Problems (5)/Tuples Problems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuples_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem 1: Return Multiple Values from a Function
+            Console.WriteLine("====== Problem 1 ======");
+            var studentInfo = GetStudentInfo();
+            Console.WriteLine($"Name: {studentInfo.Name}, Age: {studentInfo.Age}, Grade: {studentInfo.Grade}");
+
+            #endregion
+        }
+        //function for problem 1
+        static (string Name, int Age, double Grade) GetStudentInfo()
+        {
+            return ("Alice", 20, 85.5);
+        }
+    }
+}
