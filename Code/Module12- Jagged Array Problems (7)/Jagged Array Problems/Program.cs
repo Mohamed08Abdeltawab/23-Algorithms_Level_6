f6daf76a6b01679ce9748bdcc7b1d69888c6b8a2
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-08 19:35:25 +0300

    Module12 -> Problem 2: Store Sales Data by Quarter

--- Code/Module12- Jagged Array Problems (7)/Jagged Array Problems/Program.cs
+++ Code/Module12- Jagged Array Problems (7)/Jagged Array Problems/Program.cs
@@ -11,6 +11,7 @@ namespace Jagged_Array_Problems
         static void Main(string[] args)
         {
             #region Problem 1: Store Marks of Students in Multiple Subjects
+            Console.WriteLine("====== Problem 2 ======");
             int[][] jag1 = new int[][]
             {
                 new int[] { 85, 90, 78 }, // Marks for Student 1
@@ -22,6 +23,24 @@ namespace Jagged_Array_Problems
             for (int i = 0; i < jag1.Length; i++)
             {
                 Console.WriteLine($"Student {i + 1}: {string.Join(", ", jag1[i])}");
+            }
+
+            #endregion
+
+
+            #region Problem 2: Store Sales Data by Quarter
+            Console.WriteLine("====== Problem 2 ======");
+            int[][] jag2 = new int[][]
+            {
+            new int[] { 10000, 12000, 11000 },
+            new int[] { 15000, 16000 },
+            new int[] { 9000, 9500, 9800, 10200 }
+            };
+
+            for (int i = 0; i < jag2.Length; i++)
+            {
+                Console.Write($"Region {i + 1}: ");
+                Console.WriteLine(string.Join(", ", jag2[i]));
             }
 
             #endregion
