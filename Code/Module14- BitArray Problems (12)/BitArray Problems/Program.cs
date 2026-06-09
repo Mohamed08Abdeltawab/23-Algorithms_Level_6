041d6d87f3bc88b2082ed5d2d5317ddae6f0771c
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-09 12:36:56 +0300

    Module14 -> Problem4: Scheduling Tasks

--- Code/Module14- BitArray Problems (12)/BitArray Problems/Program.cs
+++ Code/Module14- BitArray Problems (12)/BitArray Problems/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,24 +25,24 @@ namespace BitArray_Problems
             #endregion
 
             #region Problem2: User Survey Results
-            Console.WriteLine("====== Problem 2 ======");
+            Console.WriteLine("\n====== Problem 2 ======");
 
             BitArray bArr2 = new BitArray(new bool[10] { false, true, false, true, false, true, false, true, false, true });
-            for(int i = 0; i< bArr2.Length; i++)
+            for (int i = 0; i < bArr2.Length; i++)
             {
-                Console.WriteLine($"User {i + 1} -> {(bArr2[i]? "Yes" : "No")}");
+                Console.WriteLine($"User {i + 1} -> {(bArr2[i] ? "Yes" : "No")}");
             }
 
             #endregion
 
             #region Problem3: Optimizing Space in Large Data
-            Console.WriteLine("====== Problem 3 ======");
+            Console.WriteLine("\n====== Problem 3 ======");
 
             BitArray bArr3 = new BitArray(1000); // 1000 bits initialized to false
             bArr3.Set(224, true); // Set the 225th bit to true
             bArr3.Set(433, true); // Set the 434th bit to true
 
-            for(int i =0; i< bArr3.Length; i++)
+            for (int i = 0; i < bArr3.Length; i++)
             {
                 if (bArr3[i])
                 {
@@ -50,6 +51,36 @@ namespace BitArray_Problems
             }
 
             #endregion
+
+
+            #region Problem4: Scheduling Tasks
+            Console.WriteLine("\n====== Problem 4 ======");
+
+            BitArray bArr4 = new BitArray(7,true); // 7 days of the week
+            bArr4.Set(0, false); // Monday
+            bArr4.Set(2, false); // Wednesday
+
+            Console.WriteLine("Scheduled tasks for the week:");
+            for (int i = 0; i < bArr4.Length; i++)
+            {
+                if (!bArr4[i])
+                {
+                    Console.WriteLine($"Day {(DaysOfWeek)i} is free!");
+                }
+            }
+
+            #endregion
+        }
+        // Enum for Problem 4
+        public enum DaysOfWeek
+        {
+            Monday = 0,
+            Tuesday = 1,
+            Wednesday = 2,
+            Thursday = 3,
+            Friday = 4,
+            Saturday = 5,
+            Sunday = 6
         }
     }
 }
