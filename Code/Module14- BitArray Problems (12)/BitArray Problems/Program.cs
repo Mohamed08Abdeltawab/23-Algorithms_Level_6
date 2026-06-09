2c2eb31ce5e612c01265da81cf5f3868899c87a6
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-09 12:15:50 +0300

    Module14 -> Problem1: Light Control System

---
+++ Code/Module14- BitArray Problems (12)/BitArray Problems/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem1: Light Control System
+            Console.WriteLine("====== Problem 1 ======");
+            BitArray bArr1 = new BitArray(new bool[] { false, true, false, true, false, true, false, true });
+
+            Console.WriteLine(string.Join(", ", bArr1.Cast<bool>()));
+
+            Console.WriteLine("After turn off light");
+            bArr1.SetAll(false);
+            Console.WriteLine(string.Join(", ", bArr1.Cast<bool>()));
+
+            #endregion
+        }
+    }
+}
