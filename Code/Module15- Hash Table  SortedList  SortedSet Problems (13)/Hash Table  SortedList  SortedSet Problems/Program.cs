f4a45fa73f194737391b30db5138f21af2293474
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-10 12:59:36 +0300

    Module15 ->  Problem3: Find Missing Numbers in a Range

--- Code/Module15- Hash Table  SortedList  SortedSet Problems (13)/Hash Table  SortedList  SortedSet Problems/Program.cs
+++ Code/Module15- Hash Table  SortedList  SortedSet Problems (13)/Hash Table  SortedList  SortedSet Problems/Program.cs
@@ -38,13 +38,26 @@ namespace Hash_Table__SortedList__SortedSet_Problems
 
             List<int> list2 = new List<int>() { 5, 3, 8, 1, 2, 5, 3 };
 
-            SortedSet<int> sortedList2 = new SortedSet<int>(list2);
+            SortedSet<int> sortedSet2 = new SortedSet<int>(list2);
 
-            foreach (int item in sortedList2)
+            foreach (int item in sortedSet2)
             {
                 Console.WriteLine(item);
             }
 
+            #endregion
+
+
+            #region Problem3: Find Missing Numbers in a Range
+            Console.WriteLine("\n====== Problem 3 ======");
+            SortedSet<int> sortedSet3 = new SortedSet<int>() { 1, 2, 4, 6, 7 };
+            for(int i = 1; i <= 7; i++)
+            {
+                if (!sortedSet3.Contains(i))
+                {
+                    Console.WriteLine("Missing number: " + i);
+                }
+            }
 
             #endregion
         }
