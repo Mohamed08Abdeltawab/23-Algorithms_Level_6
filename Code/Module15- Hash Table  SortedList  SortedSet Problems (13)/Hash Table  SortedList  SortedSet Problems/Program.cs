ac48d4b941bc1f833f2820d5593216864a5b5216
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-06-10 12:54:01 +0300

    Module15 -> Problem1: Copy Hashtable to Another Hashtable

---
+++ Code/Module15- Hash Table  SortedList  SortedSet Problems (13)/Hash Table  SortedList  SortedSet Problems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Table__SortedList__SortedSet_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem1: Copy Hashtable to Another Hashtable
+            Console.WriteLine("====== Problem 1 ======");
+
+            Hashtable ht1_1 = new Hashtable()
+            {
+                {"A", 1},
+                {"B", 2},
+                {"C", 3},
+                {5, "D"},
+                {6, "E"},
+            };
+
+            Hashtable ht1_2 = new Hashtable(ht1_1);
+
+            foreach(DictionaryEntry item in ht1_2)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+
+            #endregion
+        }
+    }
+}
